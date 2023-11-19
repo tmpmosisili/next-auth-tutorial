6582c0f544f931807d942f20f67bcb179007ea94
Thabo Mosisili <thabo.mosisili@fnb.co.za>
2023-11-19 14:24:55 +0200

    Update of code

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -5,44 +5,44 @@ export const options = {
     providers: [
         GitHubProvider({
             profile(profile) {
-                console.log("Profile GitHub: ", profile);
-                let userRole = "GitHub User";
-                if (profile?.email == "tmpmosisili@gmail.com") {
-                  userRole = "admin";
-                }
-                return {
-                    ...profile,
-                    role: userRole,
-                  };
+              console.log("Profile GitHub: ", profile);
+      
+              let userRole = "GitHub User";
+              if (profile?.email == "jake@claritycoders.com") {
+                userRole = "admin";
+              }
+      
+              return {
+                ...profile,
+                role: userRole,
+              };
             },
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_Secret,
-            
-        }),
-        GoogleProvider({
+          }),
+          GoogleProvider({
             profile(profile) {
-                console.log("Profile Google: ", profile);
-
-                let userRole = "Google User";
-
-                return {
-                    ...profile,
-                    id: profile.sub,
-                    role: userRole,
-                  };
+              console.log("Profile Google: ", profile);
+      
+              let userRole = "Google User";
+              return {
+                ...profile,
+                id: profile.sub,
+                role: userRole,
+              };
             },
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_Secret,
-        })
+          }),
     ],
     callbacks: {
         async jwt({ token, user }) {
-            if (user) token.role = user.role;
-            return token;
-          },
+          if (user) token.role = user.role;
+          return token;
+        },
         async session({ session, token }) {
           if (session?.user) session.user.role = token.role;
           return session;
         },
-    },
+      },
 };
